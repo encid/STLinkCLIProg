04edbef4eac227d4b492dbb48f2a845aa97696c4
encid <ryancav@gmail.com>
2017-06-05 09:06:21 -0400

    Initial commit

--- STLinkCLIProg/Form1.cs
+++ STLinkCLIProg/Form1.cs
@@ -47,5 +47,61 @@ namespace WindowsFormsApplication1
 
             p.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string STPath = @"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe";
+            string firmwareLocation = @"H:\Fetco\cbs2030_3015_w_bl.hex";
+
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+            {
+                proc.EnableRaisingEvents = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                //proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.Verb = "open";
+                proc.StartInfo.FileName = STPath;
+                proc.StartInfo.Arguments = "-p " + firmwareLocation + " -v";
+                proc.Start();
+                String sLine = "";
+                while ((sLine = proc.StandardOutput.ReadLine()) != null)
+                {
+                    //System.Console.WriteLine(sLine);
+                    tb.AppendText(sLine);
+                }
+                proc.WaitForExit(); //Jon Skeet was here!
+                var errorCode = proc.ExitCode;
+                proc.Close();
+            }
+
+        }
+
+
+        void runCommand()
+        {
+            string STPath = @"C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe";
+            string firmwareLocation = @"H:\Fetco\cbs2030_3015_w_bl.hex";
+
+            //* Create your Process
+            Process process = new Process();
+            process.StartInfo.FileName = STPath;
+            process.StartInfo.Arguments = "-p " + firmwareLocation + " -v";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            //* Set your output and error (asynchronous) handlers
+            process.OutputDataReceived += (s, e) => tb.AppendText("\n" + e.Data);
+            process.ErrorDataReceived += (s, e) => tb.AppendText("\n" + e.Data);
+            //* Start process and handlers
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            runCommand();
+        }
     }
 }
